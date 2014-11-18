442cc57a6dfe858b5db14f0fde32af2e0ee928aa
tjcccc <tttjjjccc@gmail.com>
2014-11-19 00:10:21 +0800

    完成通用文章页面（+/-侧边栏）页面。

--- dev/js/magicodes-home.js
+++ dev/js/magicodes-home.js
@@ -23,6 +23,35 @@
 
 
 /* ========================================
+ * 通用页面
+ * fixed footer-sole
+ * ======================================== */
+
++function ($) {
+  'use strict';
+  
+  // 当footer-sole为fixed时，使其在页面底部与之前内容的距离如同非fixed一样高。
+  if ($(".footer-sole").css("position") === "fixed") {
+    
+    var fPH, fsPH;
+    
+    // footer中container的padding-bottom值
+    fPH = $(".footer").children(".container").css("padding-bottom").slice(0, $(".footer").children(".container").css("padding-bottom").length - 2);
+    
+    // footer-sole的高度
+    fsPH = $(".footer-sole").outerHeight();
+    
+    // 将footer-sole的高度加到footer的padding-bootm上去
+    $(".footer").children(".container").css("padding-bottom", Number(fPH) + Number(fsPH) + "px");
+
+  }
+  
+}(jQuery);
+
+
+
+
+/* ========================================
  * Homepage
  * 顶部 topbar & 底部 footer 背景变色
  * ======================================== */
@@ -40,16 +69,17 @@
       $(".topbar").css("background", "none");
     }
     
-    /* footer */
-    if ($(window).scrollTop() > 1) {
-      $(".footer").css("background", "rgba(0, 0, 0, 0.8)");
-    }
-    if ($(window).scrollTop() < 1) {
-      $(".footer").css("background", "none");
+    /* footer */    
+    if ($("body").hasClass("m-home")) {
+      if ($(window).scrollTop() > 1) {
+        $(".footer-sole").css("background", "rgba(0, 0, 0, 0.8)");
+      }
+      if ($(window).scrollTop() < 1) {
+        $(".footer-sole").css("background", "none");
+      }
     }
     
   });
-  
 
 }(jQuery);
 
@@ -124,3 +154,67 @@
   });
   
 }(jQuery);
+
+
+
+
+/* ========================================
+ * 通常页面
+ * 侧边 aside
+ * ======================================== */
+
++function ($) {
+  'use strict';
+  
+  
+  // 侧边栏菜单的展开与收缩
+  
+  var $as, $asLa;
+  $as = $(".aside");
+  $asLa = $as.children("ul").children("li").children("a");
+
+  $asLa.each(function() {
+    $(this).click(function () {
+      $(this).siblings("ul").slideToggle();
+    });
+  });
+  
+  
+  // 页面滚下时自动固定；滚上去自动解锁
+  
+  var asTop;
+  asTop = $(".m-page-main").offset().top - 40;
+  
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > asTop) {
+      $as.css("margin-top", -asTop);
+      $as.css("position", "fixed");
+    }
+    if ($(window).scrollTop() < asTop) {
+      $as.css("margin-top", 0);
+      $as.css("position", "static");
+    }
+  });
+
+  
+}(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
